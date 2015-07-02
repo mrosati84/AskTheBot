1e54d23634f604544da6dc3525443ddebbc2c332
Matteo Rosati <mrosati@h-art.it>
2015-07-02 18:23:46 +0200

    add post route

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.post('/', function (req, res) {
+  res.send('Hello World!');
+});
+
 var server = app.listen(process.env.PORT, function () {
 
   var host = server.address().address;
