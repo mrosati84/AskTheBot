561fc14f2f525ffaac744c50ca75c0e13c7353cd
nicholasruggeri <ruggerinicholas@gmail.com>
2015-07-22 16:50:55 +0200

    clean file and setup commands bot

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var express = require('express'),
     app = express(),
     socket = require('socket.io');
 
+var helpers = require('./helpers'),
+    events = require('./events/events');
+
 // load dotenv
 require('dotenv').load();
 
@@ -30,11 +33,6 @@ io.on('connection', function (socket) {
 
 var token = process.env.TELEGRAM_TOKEN;
 
-var votes = {
-    yes: 0,
-    no: 0
-};
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -49,110 +47,73 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-// get and display all the votes for the running poll
-app.get('/votes', function (req, res) {
-    res.send('We have: ' + votes.yes + ' yes and ' + votes.no + ' no.');
-});
-
 // main application route, POST request.
 // action is decided upon the value of req.body.message.text
 app.post('/', function (req, res) {
 
-    console.log(JSON.stringify(req.body));
-
-    var chat_id = req.body.message.chat.id, // telegram chat ID
-        text = req.body.message.text.toLowerCase(), // the text the user has written
+    var chat_id = req.body.message.chat.id,
+        user_action = req.body.message.text + " ",
         qs = {}; // object containing the query string that will be serialized
 
-    switch(text) {
-        /**
-         * RESET ALL VOTES
-         */
-        case '/reset':
-        votes.yes = 0;
-        votes.no = 0;
+    switch (helpers.messageType(req)) {
+        case 'text':
 
-        qs = {
-            chat_id: chat_id,
-            text: 'Votes reset!',
-            reply_markup: JSON.stringify({"hide_keyboard": true})
-        };
+            if (helpers.isCommand(user_action)) {
+
+                var user_command = user_action.split(' ')[0],
+                    user_parameter = user_action.substring(user_command.length+1, user_action.length);
+
+                // Commands
+                switch(user_command) {
+                    case '/help':
+                    case '/start':
+                        qs = {
+                            reply_markup: JSON.stringify({"hide_keyboard":true}),
+                            chat_id: chat_id,
+                            text: "MESSAGGIO DI BENVENUTO"
+                        };
+                        events.sendMessage(token, qs);
+                    break;
+
+                    case '/dev':
+                        qs = {
+                            reply_markup: JSON.stringify({"hide_keyboard":true}),
+                            chat_id: chat_id,
+                            text: "MESSAGGIO DEV SONO FIGHI E BELLI"
+                        };
+                        events.sendMessage(token, qs);
+                    break;
+
+
+                    case '/send':
+                        if (!user_parameter){
+                            qs = {
+                                reply_markup: JSON.stringify({"hide_keyboard": true}),
+                                chat_id: chat_id,
+                                text: "Aggiungo la tua domanda dopo il comando '/send'"
+                            };
+                            events.sendMessage(token, qs);
+                        } else {
+                            // Il parametro viene inviato alla rotta /manage - in attesa di risposta
+                        }
+                    break;
+
+
+                    default:
+                        qs = {
+                            reply_markup: JSON.stringify({"hide_keyboard":true}),
+                            chat_id: chat_id,
+                            text: "Command not found, use /help for list of commands"
+                        };
+                        events.sendMessage(token, qs);
+                }
+            }
         break;
 
-        /**
-         * START THE BOT OR START VOTING
-         */
-        case '/start':
-        case '/vote':
-        qs = {
-            reply_markup: JSON.stringify({ "keyboard": [ ["Yes"], ["No"] ] }),
-            chat_id: chat_id,
-            text: "Welcome, " + req.body.message.chat.first_name + ", please vote"
-        };
-        break;
+    };
 
-        /**
-         * GET THE VOTES RESULTS
-         */
-        case '/results':
-        qs = {
-            reply_markup: JSON.stringify({"hide_keyboard": true}),
-            chat_id: chat_id,
-            text: 'We have: ' + votes.yes + ' yes and ' + votes.no + ' no.'
-        };
-        break;
+    res.send();
 
-        /**
-         * VOTE YES
-         */
-        case 'yes':
-        qs = {
-            chat_id: chat_id,
-            text: 'You said: ' + text,
-            reply_markup: JSON.stringify({"hide_keyboard": true})
-        };
-
-        votes.yes++;
-        break;
-
-        /**
-         * VOTE NO
-         */
-        case 'no':
-        qs = {
-            chat_id: chat_id,
-            text: 'You said: ' + text,
-            reply_markup: JSON.stringify({"hide_keyboard": true})
-        };
-
-        votes.no++;
-        break;
-
-        /**
-         * UNRECOGNIZED COMMAND
-         */
-        default:
-        qs = {
-            chat_id: chat_id,
-            text: 'Say what?',
-            reply_markup: JSON.stringify({"hide_keyboard": true})
-        };
-        break;
-    }
-
-    // sent the response message (telegram message)
-    request({
-        url: 'https://api.telegram.org/bot' + token + '/sendMessage',
-        method: 'POST',
-        qs: qs
-    }, function (err, response, body) {
-        if (err) { console.log(err); return; }
-
-        console.log('Got response ' + response.statusCode);
-        console.log(body);
-
-        res.send();
-    });
 });
 
 app.get('/manage', function (req, res) {
